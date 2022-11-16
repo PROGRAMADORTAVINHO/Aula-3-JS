ad04c92fe30a09fc953d4cc61e38672145206158
JOAO OTAVIO DUARTE DE SOUZA <joao.odsouza@senacsp.edu.br>
2022-11-16 16:13:00 -0300

    first commit

---
+++ ex na aula/script.js
@@ -0,0 +1,11 @@
+function clicou(){
+    document.querySelector('.box').innerHTML = "Clicou"
+}
+
+function entrou(){
+    document.querySelector('.box').innerHTML = "Entrou"
+}
+
+function saiu(){
+    document.querySelector('.box').innerHTML = "Saiu"
+}
---
+++ parte 2 da aula/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#exemplo').innerHTML = "<h1> João Otavio </h1>"
+document.querySelector('#exemplo').innerText = 'João Otavio'
+
+ 
